30a006ac567a496b3b7c988b3a2e75c3bc384c53
Michael Parenteau <prmparenteau@aol.com>
2011-03-11 09:17:51 -0500

    Adding sinatra-static-assets extensions.
    This adds the following helper methods:
    
      * image_tag
      * stylesheet_link_tag
      * javascript_script_tag
      * link_to

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "bundler/setup"
 require "rack"
 require "sinatra"
+require 'sinatra/static_assets'
 
 # FOR BASIC AUTH USE BELOW, REPLACE ['username', 'password'] with actual credentials
 # use Rack::Auth::Basic do |username, password|
